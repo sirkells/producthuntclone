755643d02c166b61b5413c20c4026af2a367f36d
Kelechi David Igbokwe <kelechi_igbokwe@yahoo.com>
2018-05-22 16:25:51 +0200

    Added the create product page and icons

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+    # image field to be uploaded by user for the products section
+    title = models.CharField(max_length=150)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body_text = models.TextField()  # summary of the job
+    url = models.TextField()
+    no_of_votes = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):  # func that displays the title as header in admin page
+        return self.title
+
+    def summary(self):  # function to summarise the body_text so it displays olnly the first 100 chr
+        return self.body_text[:100]
+
+    def date_format(self):  # fumction to format the date to only date without time
+        return self.date.strftime('%b %e %Y')
--- products/views.py
+++ products/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 # Create your views here.
 def home(request):
     return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body_text'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            #checks if all field has bee added
+            product = Product()
+            product.title = request.POST['title'] #add title
+            product.body_text = request.POST['body_text'] #add text
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'): #checks if http:// was added to d url
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url'] #adds http to d url
+            product.icon = request.FILES['icon']# add icon
+            product.image = request.FILES['image']#add image
+            product.pub_date = timezone.datetime.now() #time and date
+            product.no_of_votes = 1# default no of vootes is 1
+            product.hunter = request.user# adds user who added the project
+            product.save()#save and add project to db
+            return redirect('/products/' + str(product.id))
+        else:
+            return render(request, 'products/create.html', {'error': 'All fields needs to be added'})
+            
+    else:
+        return render(request, 'products/create.html')
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
+    
